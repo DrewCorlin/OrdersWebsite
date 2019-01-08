da94951e3e6a8ccc01ba74825a47d137afc0c14a
Drew Corlin <andrewcorlin@gmail.com>
2019-01-08 11:27:21 -1000

    confirm modal for deleting meals

--- app/components/MealsView.js
+++ app/components/MealsView.js
@@ -10,6 +10,7 @@ var MealView = Marionette.View.extend({
         descriptionInput: '.js-description-input',
         selectMealButton: '.js-select-meal-button',
         deleteMealButton: '.js-delete-meal-button',
+        confirmDeleteMealButton: '.js-confirm-delete-meal-button',
         textContainer: '.js-text-container',
         closeModal: '.js-modal-close'
     },
@@ -17,13 +18,15 @@ var MealView = Marionette.View.extend({
     events: {
         'click @ui.selectMealButton': 'onSelectMeal',
         'click @ui.deleteMealButton': 'onDeleteMeal',
+        'click @ui.confirmDeleteMealButton': 'onConfirmDeleteMeal',
         'click @ui.textContainer': 'onClickText',
         'click @ui.closeModal': 'onModalClose'
     },
 
     serializeData: function() {
         var data = this.serializeModel();
-        data.modalTitle = "Place Order";
+        data.createOrderModalTitle = "Place Order";
+        data.deleteMealModalTitle = "Are you sure you want to delete this meal?";
         return data;
     },
 
@@ -37,10 +40,14 @@ var MealView = Marionette.View.extend({
             App.trigger('error:toast:show', response);
         });
         this.$(this.ui.descriptionInput).val(null);
+        this.onModalClose();
     },
 
-    // TODO Task-08: Use a modal for confirmation
     onDeleteMeal: function() {
+        this.$('.js-delete-meal-modal').fadeIn(150);
+    },
+
+    onConfirmDeleteMeal: function() {
         var deleteOrder = App.request('delete:meal', this.model.get('id'));
         deleteOrder.done(response => {
             App.trigger('toast:show', response);
@@ -48,19 +55,20 @@ var MealView = Marionette.View.extend({
         }).fail(response => {
             App.trigger('error:toast:show', response);
         });
+        this.onModalClose();
     },
 
     onClickText: function() {
-        this.$('.modal').fadeIn(150);
+        this.$('.js-create-order-modal').fadeIn(150);
     },
 
     onModalClose: function() {
-        this.$('.modal').fadeOut(150);
+        this.$('.js-create-order-modal').fadeOut(150);
+        this.$('.js-delete-meal-modal').fadeOut(150);
     }
 });
 
-// TODO Task-08: Use a modal to collect the data for creating a meal,
-// then also use that modal in other places (ie. for a meal description)
+// TODO Task-08: Use a modal to collect the data for creating a meal
 export default Marionette.CompositeView.extend({
     template: tpl, // TODO Task-07: Use a more low profile div with a plus sign rather than a button
     childView: MealView,
--- app/templates/meal.tpl
+++ app/templates/meal.tpl
@@ -12,14 +12,24 @@
     <div class="js-delete-meal-button delete-meal-button">x</div>
     <!-- TODO Task-03: Use roles to restrict the delete button only to chefs -->
 </div>
-<div class="modal hidden">
+<div class="js-create-order-modal modal hidden">
     <div class="modal-content">
-        <div class="modal-title"><%- modalTitle %>
+        <div class="modal-title"><%- createOrderModalTitle %>
             <div class="js-modal-close modal-close">&times;</div>
         </div>
         <div class="meal-modal-interactions-container">
             <input type="text" name="description" placeholder="description" class="js-description-input text-input">
             <button class="js-select-meal-button btn">Select</button>
+        </div>
+    </div>
+</div>
+<div class="js-delete-meal-modal modal hidden">
+    <div class="modal-content">
+        <div class="modal-title"><%- deleteMealModalTitle %>
+            <div class="js-modal-close modal-close">&times;</div>
+        </div>
+        <div class="meal-modal-interactions-container">
+            <button class="js-confirm-delete-meal-button btn">Delete</button>
         </div>
     </div>
 </div>
